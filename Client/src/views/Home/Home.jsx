113c16d61690a0c5fb1cd9d373197d4f5f7eecc4
assaffmatias <matias.assaff98@gmail.com>
2024-02-14 22:16:41 -0300

    denominación de Landing y Home

--- Client/src/views/Home/Home.jsx
+++ Client/src/views/Home/Home.jsx
@@ -1,38 +1,11 @@
 import style from './Home.module.css'
-import one from '../../../public/MacBook-Pro-M1.png'
-import two from '../../../public/MacBook-Pro-M1-2.png'
-import three from '../../../public/MacBook-Pro-M1-3.png'
 
-const Home = () => {
-  return (
-    <div className={style.container}>
-      <div className={style.section}>
-        <div className={style.productOne}>
-          <img className={style.productsFirst} src={one} alt="" />
-          <div className={`${style.info} ${style.firstInfo}`}>
-            <p className={style.p}>Product Info</p>
-            <button className={style.button}>$20.00 USD</button>
-          </div>
+const Products = () => {
+    return(
+        <div>
+            Products Page
         </div>
-        <div className={style.leftContent}>
-          <div className={style.productTwo}>
-            <img className={style.products} src={two} alt="" />
-            <div className={style.info}>
-              <p className={style.p}>Product Info</p>
-              <button className={style.button}>$20.00 USD</button>
-            </div>
-          </div>
-          <div className={style.productThree}>
-            <img className={style.products} src={three} alt="" />
-            <div className={style.info}>
-              <p className={style.p}>Product Info</p>
-              <button className={style.button}>$20.00 USD</button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
+    )
 }
 
-export default Home;
+export default Products;
--- Client/src/views/index.js
+++ Client/src/views/index.js
@@ -3,6 +3,6 @@ import Create from './Create/Create'
 import Detail from './Detail/Detail'
 import Error from './Error/Error'
 import Home from './Home/Home'
-import Products from './Products/Products'
+import Landing from './Landing/Landing'
 
-export {About, Create, Detail, Error, Home, Products}
+export {About, Create, Detail, Error, Home, Landing}
