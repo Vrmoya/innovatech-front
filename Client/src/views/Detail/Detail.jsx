2ebcef75ec5f81ca1ae1b5657e832d772d40ddca
MelinaZarateB <melinazarate77@gmail.com>
2024-02-16 13:08:34 -0300

    Modificacion de logica para renderizado de Detail

--- Client/src/views/Detail/Detail.jsx
+++ Client/src/views/Detail/Detail.jsx
@@ -3,47 +3,44 @@ import axios from 'axios';
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import products from '../../data'
-import imageIpad from '../../assets/ipad-dorso-anverso.svg'
+import { useState } from 'react';
 
 const Detail = () => {
-  const product = {
-    category: "tablet",
-    model: "iPad Pro",
-    processor: "Apple M1",
-    screen: "12.9 inches",
-    hardcore: "256GB",
-    "use-type": "Tablet",
-    Ram: "8GB",
-    "video-card": "Apple M1",
-    price: "$1099",
-    "operating-sistem": "iPadOS",
-    dimensions: "28.06 x 21.49 x 0.59 cm",
-    weight: "682 grams",
-    "battery-life": "Up to 10 hours",
-    connectivity: "Wi-Fi, Bluetooth",
-    warranty: "12 months",
-    description: "The iPad Pro offers a seamless user experience with its sleek design and intuitive interface. Its large 12.9-inch display provides vibrant visuals, while the powerful M1 processor ensures smooth performance for all tasks. With up to 10 hours of battery life, it's perfect for work or entertainment on the go.",
-    image: imageIpad
-  };
+  const { id } = useParams()
+  const [productRender, setProductRender] = useState(null);
+
+  useEffect(() => {
+    const searchProduct = async () => {
+      const filteredProduct = await products?.filter((product) => product.id === Number(id));
+      setProductRender(filteredProduct[0]);
+    }
+    if (id) {
+      searchProduct();
+    }
+    
+      }, [id])
 
   return (
     <>
     <div className='container-fluid contenedor'>
     <div className={styles.detailContainer}>
-      <img src={imageIpad} alt={product.name} className={styles.productImage} />
-     
-      <div className={styles.productInfo}> 
-      <h1 className={styles.productTittle}>{product.model}</h1>
-      <p className={styles.productPrice}>{product.price}</p>
-      <hr />
-      <p className={styles.productDescription}>{product.description}</p>
-      <button className={styles.buttonCart}>
-      <i className="bi bi-plus"></i>
-        Add to Cart
-        </button>
-        </div> 
+      {productRender && (
+      <>
+      <img src={productRender.image} alt={productRender.name} className={styles.productImage} />
+      <div className={styles.productInfo}>
+              <h1 className={styles.productTittle}>{productRender.model}</h1>
+              <p className={styles.productPrice}>{productRender.price} USD</p>
+              <hr />
+              <p className={styles.productDescription}>{productRender.description}</p>
+              <hr />
+            <button className={styles.buttonCart}>
+                  <i className="bi bi-plus"></i>
+                  Add to Cart
+                </button>
+            </div>
+            </> 
+      )}
     </div>
-    
     </div>
     </>
   )
