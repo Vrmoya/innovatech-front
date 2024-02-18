4453f3dafbc817916e934bcac3924a95dded4568
MelinaZarateB <melinazarate77@gmail.com>
2024-02-17 21:39:56 -0300

    Detail modificado, error corregido

--- Client/src/views/Detail/Detail.jsx
+++ Client/src/views/Detail/Detail.jsx
@@ -1,19 +1,19 @@
 import styles from "./Detail.module.css";
-import axios from "axios";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import products from "../../data";
 import { useState } from "react";
 import CarouselDetail from "../../components/CarouselDetail/CarouselDetail";
 import { getProductById } from "../../redux/actions";
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Detail = () => {
   const { id } = useParams();
-  console.log(id)
+  console.log(id);
   const [productRender, setProductRender] = useState(null);
-  const dispatch = useDispatch()
-  const productById = useSelector((state) => state.getProductById)
+  const dispatch = useDispatch();
+  const productById = useSelector((state) => state.getProductById);
 
   useEffect(() => {
     // dispatch(getProductById(id))
@@ -24,13 +24,13 @@ const Detail = () => {
       setProductRender(filteredProduct[0]);
     };
     if (id) {
-      searchProduct()
+      searchProduct();
     }
-  }, [id]) // Agregar productRender como dependencia
+  }, []); // Agregar productRender como dependencia
 
   let productoFiltrado;
-  if(productRender){
-   productoFiltrado = Object.fromEntries(
+  if (productRender) {
+    productoFiltrado = Object.fromEntries(
       Object.entries(productRender).filter(
         ([key, value]) =>
           value !== null &&
@@ -40,58 +40,62 @@ const Detail = () => {
       )
     );
   }
+  console.log(productoFiltrado);
 
   return (
     <>
-    <div className="container-fluid contenedor">
-      <div className={styles.detailContainer}>
-        {productoFiltrado && (
-          <>
-            {Object.entries(productoFiltrado).map(([key, value]) => (
-              <>
-                {key === "image" && (
-                  <img src={value} alt="Product Image" className={styles.productImage} />
-                )}
-                <div className={styles.productInfo}></div>
-                {key === "model" && (
-                  <h1 className={styles.productTittle}>{value}</h1>
-                )}
-                {key === "price" && (
-                  <button className={styles.productPrice}>{value} USD</button>
-                )}
-                {key === "description" && (
-                  <>
-                    <hr />
-                    <p className={styles.productDescription}>{value}</p>
-                    <hr />
-                  </>
-                )}
-                {key !== "id" && key !== "image" && key !== "model" && key !== "price" && key !== "description" && (
-                   <div className="container">
-                   <div>
-                   <ul className={`${styles.productSpecs} ${styles.circleList}`}>
-                  <li className={styles.productSpec}>
-                    <strong>{key.charAt(0).toUpperCase() + key.slice(1)}:</strong> {value}
-                  </li>
-                  </ul>
-                  </div>
-                  </div>
-                )}
-              </>
-            ))}
-            <hr />
-            <button className={styles.buttonCart}>
-              <i className="bi bi-plus"></i>
-              Add to Cart
-            </button>
-          </>
-        )}
-      </div>
-      <div className={styles.contenedorCarousel}>
-        <CarouselDetail />
-      </div>
-    </div>
-  </>
-)}
+      {productoFiltrado && (
+        <div className="container-fluid contenedor">
+          <div className={styles.detailContainer}>
+            <img
+              src={productoFiltrado.image}
+              alt={productoFiltrado.model}
+              className={styles.productImage}
+            />
+            <div className={styles.productInfo}>
+              <h1 className={styles.productTittle}>{productoFiltrado.model}</h1>
+              <button className={styles.productPrice}>
+                {productoFiltrado.price} USD
+              </button>
+              <hr />
+              <p className={styles.productDescription}>
+                {productoFiltrado.description}
+              </p>
+              <hr />
+              <div className="container">
+                <div>
+                <ul className={`${styles.productSpecs} ${styles.circleList}`}>
+                  {Object.entries(productoFiltrado).map(
+                    ([key, value]) =>
+                      key !== "id" &&
+                      key !== "image" &&
+                      key !== "model" &&
+                      key !== "price" &&
+                      key !== "description" && (
+                        <li className={styles.productSpec} key={key}>
+                          <strong>
+                            {key.charAt(0).toUpperCase() + key.slice(1)}:
+                          </strong>{" "}
+                          {value}
+                        </li>
+                      )
+                  )}
+                </ul>
+                </div>
+              </div>
+              <button className={styles.buttonCart}>
+                      <i className="bi bi-plus"></i>
+                      Add to Cart
+                    </button>
+            </div>
+          </div>
+          <div>
+            <CarouselDetail />
+            </div>
+        </div>
+      )}
+    </>
+  );
+};
 
-export default Detail
+export default Detail;
