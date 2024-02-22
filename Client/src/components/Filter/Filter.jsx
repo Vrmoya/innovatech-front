dfb0dabc1dd29b2f4ba6cd4d9a1afae6a6c2a63c
assaffmatias <matias.assaff98@gmail.com>
2024-02-22 20:45:09 -0300

    Cambios en Nav, Filter y Order

--- Client/src/components/Filter/Filter.jsx
+++ Client/src/components/Filter/Filter.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import style from './Filter.module.css'
 import { useDispatch } from "react-redux";
 import { getCategories } from '../../redux/actions';
 
 const Filter = () => {
   const dispatch = useDispatch()
-  
+  const [selected, setSelected] = useState("")
 
   const handleFilter = (category) => {
+    setSelected(category)
     dispatch(getCategories(category))
   };
 
@@ -15,12 +16,12 @@ const Filter = () => {
 
     <div className={style.container}>
       <h3 className={style.title}>Collections</h3>
-      <button className={style.button} onClick={() => handleFilter("")}>All</button>
-      <button className={style.button} onClick={() => handleFilter("tablet")}>Tablets</button>
-      <button className={style.button} onClick={() => handleFilter("laptop")}>Laptops</button>
-      <button className={style.button} onClick={() => handleFilter("smartphone")}>SmartPhones</button>
-      <button className={style.button} onClick={() => handleFilter("headphone")}>Headphones</button>
-      <button className={style.button} onClick={() => handleFilter("keyboard")}>Keyboards</button>
+      <button className={`${style.button} ${selected === '' && style.selected}`} onClick={() => handleFilter("")}>All</button>
+      <button className={`${style.button} ${selected === 'tablet' && style.selected}`} onClick={() => handleFilter("tablet")}>Tablets</button>
+      <button className={`${style.button} ${selected === 'laptop' && style.selected}`} onClick={() => handleFilter("laptop")}>Laptops</button>
+      <button className={`${style.button} ${selected === 'smartphone' && style.selected}`} onClick={() => handleFilter("smartphone")}>SmartPhones</button>
+      <button className={`${style.button} ${selected === 'headphone' && style.selected}`} onClick={() => handleFilter("headphone")}>Headphones</button>
+      <button className={`${style.button} ${selected === 'keyboard' && style.selected}`} onClick={() => handleFilter("keyboard")}>Keyboards</button>
     </div>
   )
 }
