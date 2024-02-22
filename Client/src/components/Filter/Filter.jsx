bd5062b15cab4baea958f0ae6fe3afad5797b5d7
GeraAmaya <gerardoamaya.10@gmail.com>
2024-02-22 20:24:53 -0300

    *

--- Client/src/components/Filter/Filter.jsx
+++ Client/src/components/Filter/Filter.jsx
@@ -1,28 +1,59 @@
-import React from 'react'
-import style from './Filter.module.css'
-import { useDispatch } from "react-redux";
+import React, { useState } from 'react';
+import style from './Filter.module.css';
+import { useDispatch } from 'react-redux';
 import { getCategories } from '../../redux/actions';
 
 const Filter = () => {
-  const dispatch = useDispatch()
-  
+  const dispatch = useDispatch();
+  const [selectedCategory, setSelectedCategory] = useState('');
 
   const handleFilter = (category) => {
-    dispatch(getCategories(category))
+    dispatch(getCategories(category));
+    setSelectedCategory(category);
   };
 
   return (
-
     <div className={style.container}>
       <h3 className={style.title}>Collections</h3>
-      <button className={style.button} onClick={() => handleFilter("")}>All</button>
-      <button className={style.button} onClick={() => handleFilter("tablet")}>Tablets</button>
-      <button className={style.button} onClick={() => handleFilter("laptop")}>Laptops</button>
-      <button className={style.button} onClick={() => handleFilter("smartphone")}>SmartPhones</button>
-      <button className={style.button} onClick={() => handleFilter("headphone")}>Headphones</button>
-      <button className={style.button} onClick={() => handleFilter("keyboard")}>Keyboards</button>
+      <button
+        className={`${style.button} ${selectedCategory === '' ? style.selected : ''}`}
+        onClick={() => handleFilter('')}
+      >
+        All
+      </button>
+      <button
+        className={`${style.button} ${selectedCategory === 'tablet' ? style.selected : ''}`}
+        onClick={() => handleFilter('tablet')}
+      >
+        Tablets
+      </button>
+      <button
+        className={`${style.button} ${selectedCategory === 'laptop' ? style.selected : ''}`}
+        onClick={() => handleFilter('laptop')}
+      >
+        Laptops
+      </button>
+      <button
+        className={`${style.button} ${selectedCategory === 'smartphone' ? style.selected : ''}`}
+        onClick={() => handleFilter('smartphone')}
+      >
+        SmartPhones
+      </button>
+      <button
+        className={`${style.button} ${selectedCategory === 'headphone' ? style.selected : ''}`}
+        onClick={() => handleFilter('headphone')}
+      >
+        Headphones
+      </button>
+      <button
+        className={`${style.button} ${selectedCategory === 'keyboard' ? style.selected : ''}`}
+        onClick={() => handleFilter('keyboard')}
+      >
+        Keyboards
+      </button>
     </div>
-  )
-}
+  );
+};
 
 export default Filter;
+
--- Client/src/components/Order/Order.jsx
+++ Client/src/components/Order/Order.jsx
@@ -38,3 +38,6 @@ const Order = () => {
 };
 
 export default Order;
+
+
+
