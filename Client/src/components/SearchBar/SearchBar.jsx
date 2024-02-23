392d3340f035d3a44693d49251407126e17a1619
GeraAmaya <gerardoamaya.10@gmail.com>
2024-02-23 00:46:22 -0300

    input, filtros

--- Client/src/components/SearchBar/SearchBar.jsx
+++ Client/src/components/SearchBar/SearchBar.jsx
@@ -1,5 +1,5 @@
 import style from "./SearchBar.module.css";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { filterByModel, getProducts } from "../../redux/actions";
 import { useNavigate } from 'react-router-dom';
@@ -13,24 +13,34 @@ const SearchBar = () => {
 
   const handleChange = (event) => {
     event.preventDefault();
-    console.log(event.target.value);
     setModel(event.target.value);
   };
+
+  const handleClear = () => {
+    // Limpiar el estado del modelo y obtener todos los productos
+    setModel("");
+    dispatch({ type: 'CHANGE_MODEL', payload: "" });
+    dispatch(getProducts(categories, order));
+  };
+
   const handleSubmit = () => {
-    dispatch({type:'CHANGE_MODEL',payload:model});
-    // setModel("");
+    dispatch({ type: 'CHANGE_MODEL', payload: model });
     navigate('/home');
   };
+
   const handleKeyDown = (event) => {
     if (event.key === "Enter") {
       handleSubmit();
     }
   };
-  
 
   return (
-    <div className={style.group}>  
-        <svg className={style.icon} onClick={() => handleSubmit()} aria-hidden="true" viewBox="0 0 24 24"><g><path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path></g></svg>
+    <div className={style.group}>
+      <svg className={style.icon} onClick={handleSubmit} aria-hidden="true" viewBox="0 0 24 24">
+        <g>
+          <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path>
+        </g>
+      </svg>
       <input
         className={style.input}
         placeholder="Search for products..."
@@ -38,8 +48,16 @@ const SearchBar = () => {
         onKeyDown={handleKeyDown}
         onChange={handleChange}
       />
-    </div>
+     
+      {model && (
+        <button className={style.clearButton} onClick={handleClear}>
+          X
+        </button>
+      )}
+      </div>
+    
   );
 };
 
 export default SearchBar;
+
