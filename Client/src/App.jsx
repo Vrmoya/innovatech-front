e5728826f6668d2e937c30fc2c457ef065e809aa
assaffmatias <matias.assaff98@gmail.com>
2024-02-22 21:05:51 -0300

    footer fix

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,6 +2,7 @@ import PATHROUTES from "./helpers/PathRoutes";
 import { Routes, Route } from 'react-router-dom';
 import {About, Dashboard, Detail, Error, Home, Landing} from './views'
 import NavBar from "./components/NavBar/NavBar";
+import Footer from "./components/Footer/Footer";
 
 function App() {
 
@@ -16,6 +17,7 @@ function App() {
         <Route path={PATHROUTES.HOME} element={<Home />}/>
         <Route path={PATHROUTES.LANDING} element={<Landing />}/>
       </Routes>
+      <Footer />
     </div>
   )
 }
---
+++ Client/src/components/Footer/Footer.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import PATHROURES from '../../helpers/PathRoutes';
+import logo from '../../../public/logo.png'
+import style from './Footer.module.css'; // Asegúrate de tener el archivo de estilos correspondiente
+
+const Footer = () => {
+    return (
+        <footer className={style.footer}>
+
+            <div className={style.linkContainer}>
+                <Link to={PATHROURES.LANDING}>
+                    <img src={logo} alt="" className={style.img} />
+                </Link>
+                <Link to={PATHROURES.LANDING} className={style.title}>INNOVA TECH</Link>
+
+                <nav className={style.nav}>
+                    <Link to={PATHROURES.HOME} className={style.link}  >Home</Link>
+                    <Link to={PATHROURES.ABOUT} className={style.link} >About</Link>
+                </nav>
+
+            </div>
+
+            <div className={style.text}>
+                <p>© 2024 Innova Tech. All rights reserved.</p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
