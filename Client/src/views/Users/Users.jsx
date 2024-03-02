4e342f531f9cf709036c26a4dc024262a3a592c8
Franco Tomassich <francotomassich@gmail.com>
2024-03-02 17:32:34 -0300

    creación de vista users

---
+++ Client/src/views/Users/Users.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from "./Users.module.css";
+
+const Users = () => {
+  return (
+    <div className={style=containerView}>Users</div>
+  )
+}
+
+export default Users
